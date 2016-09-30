2692297238c88da40250570e1de6b7f541ea9524
Ye Lin Aung <me@yelinaung.com>
2016-09-30 11:34:58 +0800

    fix(fmt): run gofmt

--- process.go
+++ process.go
@@ -1,68 +1,67 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "image"
-    "strconv"
-    "image/color"
-    "github.com/disintegration/imaging"
+	"fmt"
+	"github.com/disintegration/imaging"
+	"image"
+	"image/color"
+	"strconv"
+	"time"
 )
 
-
 type Size struct {
-    w int
-    h int
+	w int
+	h int
 }
 
 var all []Size
 
 func main() {
-    start := time.Now()
+	start := time.Now()
 
-    all := []Size{
-        Size{
-            w: 100,
-            h: 100,
-        },
-        Size{
-            w: 200,
-            h: 200,
-        },
-        Size{
-            w: 300,
-            h: 300,
-        },
-        Size{
-            w: 400,
-            h: 400,
-        },
-        Size{
-            w: 500,
-            h: 500,
-        },
-    }
+	all := []Size{
+		Size{
+			w: 100,
+			h: 100,
+		},
+		Size{
+			w: 200,
+			h: 200,
+		},
+		Size{
+			w: 300,
+			h: 300,
+		},
+		Size{
+			w: 400,
+			h: 400,
+		},
+		Size{
+			w: 500,
+			h: 500,
+		},
+	}
 
-    // input file
-    file := "img/eg.jpeg"
+	// input file
+	file := "img/eg.jpeg"
 
-    img, err := imaging.Open(file)
-    if err != nil {
-       panic(err)
-    }
+	img, err := imaging.Open(file)
+	if err != nil {
+		panic(err)
+	}
 
-    // Generate all resized image sizes to output folder
-    for _, s := range all {
-        r := imaging.Resize(img, s.w, s.h, imaging.Lanczos)
-        dst := imaging.New(s.w, s.h, color.NRGBA{0, 0, 0, 0})
-        dst = imaging.Paste(dst, r, image.Pt(0, 0))
-        err := imaging.Save(dst, "output/dst" + strconv.Itoa(s.w) + strconv.Itoa(s.h) +".jpg")
-        if err != nil {
-            panic(err)
-        }
-    }
+	// Generate all resized image sizes to output folder
+	for _, s := range all {
+		r := imaging.Resize(img, s.w, s.h, imaging.Lanczos)
+		dst := imaging.New(s.w, s.h, color.NRGBA{0, 0, 0, 0})
+		dst = imaging.Paste(dst, r, image.Pt(0, 0))
+		err := imaging.Save(dst, "output/dst"+strconv.Itoa(s.w)+strconv.Itoa(s.h)+".jpg")
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    elapsed := time.Since(start)
+	elapsed := time.Since(start)
 
-    fmt.Println("Resize timing millisecond : " + strconv.Itoa(int(elapsed.Nanoseconds()/1000000)));
+	fmt.Println("Resize timing millisecond : " + strconv.Itoa(int(elapsed.Nanoseconds()/1000000)))
 }
